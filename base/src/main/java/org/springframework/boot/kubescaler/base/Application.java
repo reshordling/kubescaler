9c4c2c49794c0473e90e7eaee27fa1828d0a0c95
Vasily Tsvetkov <vasily.tsvetkov@roguewave.com>
2019-06-27 01:28:15 +0300

    remove unused endpoints from base

--- base/src/main/java/org/springframework/boot/kubescaler/base/Application.java
+++ base/src/main/java/org/springframework/boot/kubescaler/base/Application.java
@@ -5,24 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.kubescaler.base.configuration.RibbonConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
-@RibbonClient(name = "base", configuration = RibbonConfiguration.class)
+@RibbonClient(name = "base-service", configuration = RibbonConfiguration.class)
 public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
-  }
-
-  @LoadBalanced
-  @Bean
-  RestTemplate restTemplate() {
-    return new RestTemplate();
   }
 }
--- base/src/main/java/org/springframework/boot/kubescaler/base/MainController.java
+++ base/src/main/java/org/springframework/boot/kubescaler/base/MainController.java
@@ -1,12 +1,5 @@
 package org.springframework.boot.kubescaler.base;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
-import org.springframework.boot.kubescaler.base.model.Profile;
-import org.springframework.boot.kubescaler.base.model.User;
-import org.springframework.boot.kubescaler.base.service.ProfileRepository;
-import org.springframework.boot.kubescaler.base.service.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,24 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-
-  final UserRepository userRepository;
-  final ProfileRepository profileRepository;
-
-  public MainController(UserRepository userRepository, ProfileRepository profileRepository) {
-    this.userRepository = userRepository;
-    this.profileRepository = profileRepository;
-  }
-
-  @RequestMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
-  public ResponseEntity index() {
-    User user1 = userRepository.create(User.builder().build());
-    User user2 = userRepository.create(User.builder().build());
-    User user3 = userRepository.create(User.builder().build());
-    Profile profile1 = profileRepository.create(Profile.builder().users(Set.of(user1.getId(), user2.getId(), user3.getId())).build());
-    Profile profile2 = profileRepository.create(Profile.builder().users(Set.of(user1.getId(), user2.getId())).build());
-    return ResponseEntity.ok(String.format("New profiles: %s and %s, total profiles: %d", profile1, profile2, profileRepository.count()));
-  }
 
   @RequestMapping(value = "/health", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
   public ResponseEntity health() {
