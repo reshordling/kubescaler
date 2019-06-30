ea71eb40a27fc72a13ef7cebada1ac7028a3d903
Vasily Tsvetkov <vasily.tsvetkov@roguewave.com>
2019-06-30 22:15:24 +0300

    use 10 cores and increase main/base/stream cores

--- create-cluster.sh
+++ create-cluster.sh
@@ -2,7 +2,7 @@
 
 minikube delete
 minikube start \
-  --memory 8192 --cpus 4 \
+  --memory 8192 --cpus 10 \
   --extra-config=controller-manager.horizontal-pod-autoscaler-upscale-delay=10s  \
   --extra-config=controller-manager.horizontal-pod-autoscaler-downscale-delay=2m \
   --extra-config=controller-manager.horizontal-pod-autoscaler-sync-period=10s
