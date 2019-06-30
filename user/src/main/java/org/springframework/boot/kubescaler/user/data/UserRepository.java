722bf4da2d496f7f31e2e53ce58acd727282497b
Vasily Tsvetkov <vasily.tsvetkov@roguewave.com>
2019-06-30 16:31:03 +0300

    delete scaffolding 'user'

--- user/src/main/java/org/springframework/boot/kubescaler/user/data/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.springframework.boot.kubescaler.user.data;
-
-import java.util.UUID;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, UUID> {
-}
